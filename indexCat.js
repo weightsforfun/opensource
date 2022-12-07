92f992e734c1ddbf38939331540f3f719a09c2f6
Yg-Kim <ygk0951@hanyang.ac.kr>
2022-12-07 15:37:59 +0900

    Apply move function

--- indexCat.js
+++ indexCat.js
@@ -3,22 +3,57 @@ var WIDTH = 1080;
 var HEIGHT = 750;
 var PADDING = 50;
 var xPos = 0, yPos = 0;
-var xPre = 0, yPre = 0;
+var xPosInit = 0, yPosInit = 0;
+var arrived = true;
+var amount = 1;
+var ms = 30;
 
 window.onload = function() {
-    // load call back
+    initializeCat();
     catPlaying();
 };
 
-// only for test
+function initializeCat(){
+    // choice random cat
+    // var randomCat = Math.round(Math.random() * 2);
+    // console.log(randomCat);
+    // switch(randomCat){
+    //     case 0:
+    //         catGif.src = "./picture/cat_gif.gif";
+    //         break;
+    //     case 1:
+    //         catGif.src = "./picture/cat_gif2.gif";
+    //         break;
+    //     default:
+    //         catGif.src = "./picture/cat_gif_ref.gif";
+    //         break;
+    // }
+
+    xPosInit = generateRandomPosition(PADDING, WIDTH);
+    yPosInit = generateRandomPosition(PADDING, HEIGHT);
+    catGif.style.left = xPosInit + "px";
+    catGif.style.top = yPosInit + "px";
+    console.log("init pos : (" + xPosInit + ", " + yPosInit + ")");
+}
+
+// 20ms마다 고양이 방향, 위치 갱신
 function catPlaying(){
-    setInterval(function() {
-        getNewPosition(WIDTH, HEIGHT);
-        setNewPosition();
-        setDirection();
-        // move();
-        setPrePosition();
-    }, 2000);
+    var catWalk = setInterval(() => move(amount), ms);
+}
+
+function move(amount){
+    if(arrived){
+        getNewPosition(WIDTH, HEIGHT);  // 목적지
+        setDirection();                 // 방향
+        arrived = false;
+    }
+    // console.log("currnct coordinate : (" + getNowPosX() + ", " + getNowPosY() + ")");
+    // 이동
+    if(getNowPosX() < xPos) catGif.style.left = getNowPosX() + amount + "px";
+    else if(getNowPosX() > xPos) catGif.style.left = getNowPosX() - amount + "px";
+    if(getNowPosY() < yPos) catGif.style.top = getNowPosY() + amount + "px";
+    else if(getNowPosY() > yPos) catGif.style.top = getNowPosY() - amount + "px";
+    if(getNowPosX() == xPos && getNowPosY() == yPos) arrived = true;
 }
 
 function getDistance(ax, ay, zx, zy){
@@ -29,11 +64,11 @@ function getDistance(ax, ay, zx, zy){
 }
 
 function generateRandomPosition(padding, boundary){
-    return padding + Math.floor(Math.random() * boundary - padding);
+    return padding + Math.floor(Math.random() * boundary);
 }
 
 function getDirection(){
-    if (xPre <= xPos)
+    if (getNowPosX() <= xPos)
         return -1;
     else
         return 1;
@@ -46,9 +81,7 @@ function setDirection(){
 function getNewPosition(width, height){
     xPos = generateRandomPosition(PADDING, width);
     yPos = generateRandomPosition(PADDING, height);
-    console.log("Distance : "+Math.round(getDistance(xPos, yPos, xPre, yPre)));
-    console.log("X position : " + xPos);
-    console.log("Y position : " + yPos);
+    console.log("New Position : (" + xPos + ", " + yPos + ")");
 }
 
 function setNewPosition(){
@@ -56,11 +89,10 @@ function setNewPosition(){
     catGif.style.top = yPos + "px";
 }
 
-function setPrePosition(){
-    xPre = xPos;
-    yPre = yPos;
+function getNowPosX(){
+    return parseInt(catGif.style.left.replace('px', ''));
 }
 
-function move(){
-    // setInterval(() => console.log("test"), 20);
+function getNowPosY(){
+    return parseInt(catGif.style.top.replace('px', ''))
 }
